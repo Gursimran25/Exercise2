a73caf6cbde8a408d316c3b20e6acdb301a05ae9
Gursimran Kaur Kang <kang7@sheridancollege.ca>
2021-06-11 22:54:15 -0400

    Final solution

--- src/main/resources/footer.js
+++ src/main/resources/footer.js
@@ -1,6 +1,6 @@
 $(function () {
 
     $("footer")
-        .html("Sheridan College&nbsp;&nbsp;<span class='hart'>&#9825;</span>&nbsp;&nbsp;")
+        .html("Sheridan College&nbsp;&nbsp; Gursimran Kang&nbsp;&nbsp;<span class='hart'>&#9825;</span>&nbsp;&nbsp;")
 
 });
